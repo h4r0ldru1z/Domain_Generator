161234d071f51ddfea354f6c165889a0ef11be5a
h4r0ldru1z <haroldand.ruiz@gmail.com>
2022-08-25 03:55:39 +0000

    listos

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 console.log("Hello World");
 var pronoun = ["the", "our", "Mr", "Main"];
-var adj = ["great", "big", "pal", "very unique"];
-var noun = ["jogger", "racoon", "triathetle"];
+var adj = ["great", "big", "pal", "veryunique"];
+var noun = ["jogger", "racoon", "triathetl"];
 var ext = [".es", ".com", ".org"];
+var hacks = [];
 var dominios = [];
 
 function generadorDeDominios(parte1, parte2, parte3, parte4) {
   //let nombreDominio = 0;
+  for (let index in parte3) {
+    for (let index2 in parte4) {
+      let namehacks = parte3[index] + parte4[index2];
+      hacks.push(namehacks);
+    }
+  }
   for (let a = 0; a < parte1.length; a++) {
     for (let b = 0; b < parte2.length; b++) {
-      for (let c = 0; c < parte3.length; c++) {
-        for (let d = 0; d < parte4.length; d++) {
-          let nombreDominio = parte1[a] + parte2[b] + parte3[c] + parte4[d];
-          dominios.push(nombreDominio);
-        }
+      for (let c = 0; c < hacks.length; c++) {
+        let nombreDominio = parte1[a] + parte2[b] + hacks[c];
+        dominios.push(nombreDominio);
       }
     }
   }
 }
+
 generadorDeDominios(pronoun, adj, noun, ext);
 console.log(dominios);
